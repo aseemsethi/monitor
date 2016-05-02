22b0cef5afb26ee49f32d68ad9bb7756d5c6f871
Aseem Sethi <aseemsethi@yahoo.com>
2016-05-02 22:47:05 +0530

    Some fixes

--- monitor/ssl_perf/ssl_perf.c
+++ monitor/ssl_perf/ssl_perf.c
@@ -60,6 +60,7 @@ sslPerfGetCertInfo (sslPerfStruct_t *sslPf, xmlData_t* xmlData) {
     log_info(fp, "Error: Could not get a certificate from: %s", sslPf->selfIP);
   else
     log_info(fp, "Retrieved the sslPf.selfIP's certificate from: %s", sslPf->selfIP);
+  fflush(fp);
 
   /* ---------------------------------------------------------- *
    * extract various certificate information                    *
@@ -81,16 +82,7 @@ sslConnectToServer (sslPerfStruct_t *sslPf, xmlData_t* xmlData) {
   const SSL_METHOD *method;
   int ret, i;
 
-  /* ---------------------------------------------------------- *
-   * Create the Input/Output BIO's.                             *
-   * ---------------------------------------------------------- */
   outbio  = BIO_new_fp(stdout, BIO_NOCLOSE);
-
-  /* ---------------------------------------------------------- *
-   * initialize SSL library and register algorithms             *
-   * ---------------------------------------------------------- */
-  if(SSL_library_init() < 0)
-    BIO_printf(outbio, "Could not initialize the OpenSSL library !\n");
 
   /* ---------------------------------------------------------- *
    * Set SSLv2 client hello, also announce SSLv3 and TLSv1      *
@@ -117,15 +109,21 @@ sslConnectToServer (sslPerfStruct_t *sslPf, xmlData_t* xmlData) {
    * Attach the SSL session to the socket descriptor            *
    * ---------------------------------------------------------- */
   SSL_set_fd(sslPf->ssl, sslPf->sock);
+  printf("\nSSL: %x, sock:%d", sslPf->ssl, sslPf->sock); fflush(stdout);
 
   /* ---------------------------------------------------------- *
    * Try to SSL-connect here, returns 1 for success             *
    * ---------------------------------------------------------- */
-  if ( SSL_connect(sslPf->ssl) != 1 )
+  if (SSL_connect(sslPf->ssl) != 1)
     BIO_printf(outbio, "Error: Could not build a SSL session to: %s.\n", sslPf->selfIP);
-  else
+  else {
+	int err;
     BIO_printf(outbio, "Successfully enabled SSL/TLS session to: %s.\n", sslPf->selfIP);
-
+	if (err<1) {
+		err=SSL_get_error(sslPf->ssl,err);
+		printf("SSL error #%d in accept,program terminated\n",err);
+	}
+	}
 
   log_info(fsslPerfStats, "SSL: Connect to %s with sock: %d, sslId: %d - Pass", 
 			xmlData->serverIP, sslPf->sock, sslPf->id);
@@ -134,7 +132,9 @@ sslConnectToServer (sslPerfStruct_t *sslPf, xmlData_t* xmlData) {
 }
 
 sslPerfFreeConn (sslPerfStruct_t *sslPf, xmlData_t* xmlData) {
-	SSL_free(sslPf->ssl); close(sslPf->sock); SSL_CTX_free(sslPf->ctx);
+	SSL_free(sslPf->ssl); 
+	SSL_CTX_free(sslPf->ctx);
+	close(sslPf->sock); 
 }
 
 sslPerfCreateConn (sslPerfStruct_t *sslPf, xmlData_t* xmlData) {
@@ -151,8 +151,6 @@ sslPerfCreateConn (sslPerfStruct_t *sslPf, xmlData_t* xmlData) {
             log_error(fp, "inet_aton() failed\n");
             log_error(fp, "SSL ERROR: create in inet_aton"); fflush(fp);
     }
-    log_info(fp, "SSL: Connect to %s with sock: %d, sslId: %d", 
-			xmlData->serverIP, sslPf->sock, sslPf->id);
     if(connect(sslPf->sock, (struct sockaddr *)&sslPf->server_addr,
                 sizeof(struct sockaddr)) == -1) {
         log_error(fp, "SSL ERROR: create connecting to server"); fflush(fp);
@@ -161,8 +159,8 @@ sslPerfCreateConn (sslPerfStruct_t *sslPf, xmlData_t* xmlData) {
         perror("Connect");
         exit(1);
     }
-    log_info(fp, "TCP connection created to %s, sock:%d",
-        xmlData->serverIP, sslPf->sock);
+    log_info(fp, "SSL: Connect to %s with sock: %d, sslId: %d", 
+			xmlData->serverIP, sslPf->sock, sslPf->id);
 	fflush(fp);
 }
 
@@ -177,6 +175,8 @@ sslPerfTestsExec (xmlData_t* xmlData) {
 	ERR_load_BIO_strings();
 	ERR_load_crypto_strings();
 	SSL_load_error_strings();
+	if(SSL_library_init() < 0)
+		log_error(fp, "Could not initialize the OpenSSL library !\n");
 
 	for (i = 0; i < xmlData->sslPerSec; i++) {
 		sslPf = malloc(sizeof(sslPerfStruct_t));
@@ -185,12 +185,15 @@ sslPerfTestsExec (xmlData_t* xmlData) {
 		}
     	getOwnIP(sslPf->selfIP);
     	log_info(fp, "SSL: SelfIP: %s", sslPf->selfIP); fflush(fp);
-		sslPf->id = id++;
+		sslPf->id = i;
 		sslPfQ[id] = sslPf; // save the sslPf pointer for stats etc
 		sslPerfCreateConn(sslPf, xmlData); // updates the sock in sslPf
 		sslConnectToServer(sslPf, xmlData); // SSL Connect to Server using sslPf->sock
 		sslPerfGetCertInfo(sslPf, xmlData); 
-		sslPerfFreeConn(sslPf, xmlData); 
+		//sslPerfFreeConn(sslPf, xmlData); 
+	}
+	for (i = 0; i < xmlData->sslPerSec; i++) {
+		//sslPerfFreeConn(sslPfQ[i], xmlData); 
 	}
 }
 
